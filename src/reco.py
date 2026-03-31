fac95df69d7ba37f0c432ab3de40fadbc737c9ed
minimartzz <martin.ho.zy97@gmail.com>
2026-03-31 19:12:53 +0800

    [TRSF] transferring files

---
+++ src/reco.py
@@ -0,0 +1,9 @@
+"""
+Board Game Recommendation System
+================================
+"""
+
+# ========================================
+# Data Loading & Feature Engineering
+# ========================================
+def load_and_prepare_data(duckdb_path: str):
